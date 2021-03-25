8589beac5365651eccc5abb4de7d46a5e91a5403
Martin Halas <echoes.time@gmail.com>
2021-03-25 01:13:43 +0000

    moved relevant top-page button scripts to separate composer.js file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -120,27 +120,4 @@ $('.write-tweet').on('click', () => {
   $('.new-tweet').slideDown();
 });
 
-$('#top-page').on('mouseenter', () => {
-  $('#move-up').html('&#129304&#11014;&#11014;');
-})
-
-$('#top-page').on('mouseleave', () => {
-  $('#move-up').html('&#11014;');
-})
-
-$('#top-page').on('click' , () => {
-  window.scrollTo(0,0);
-})
-
-$(window).on('scroll', function() {
-  if ($(this).scrollTop() < 200) {
-    $('#top-page').css('display', 'none');
-    $('nav').css('display', 'flex');
-  } else {
-    $('#top-page').css('display', 'flex');
-    $('nav').css('display', 'none');
-  }
-});
-
-
 loadTweets();
